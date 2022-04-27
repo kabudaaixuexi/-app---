b99f35beac7458a477a37a10f6bb8f0456341c4a
kabudaaixuexi <kaburda@163.com>
2022-04-27 11:26:31 +0800

    build: build 0.1.2

--- core/packages/xseditor-listener/change.ts
+++ core/packages/xseditor-listener/change.ts
@@ -3,7 +3,7 @@ import { createElement } from "../xseditor-dummy/create";
 
 export default (region: Element | Target, callback: Function) => {
     region?.addEventListener("input", debounce((ev: any) => {
-            callback(region, ev, createElement('article', ev.target));
+            callback(region, createElement('article', region), ev, createElement('article', ev.target));
         }, 300, false)
     );
 }
