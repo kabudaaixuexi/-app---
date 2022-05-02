d64ed392a0a71e2d4ddd44d62c21ed1ebb016671
kabudaaixuexi <kaburda@163.com>
2022-05-02 17:23:34 +0800

    build: build 0.3.8

--- lib/packages/xseditor-kernel/index.d.ts
+++ lib/packages/xseditor-kernel/index.d.ts
@@ -1,2 +1,17 @@
-declare const _default: (Config?: Target) => any;
+declare const _default: (Config?: Target) => {
+    children: any;
+    xs_data: any;
+    xs_tag: any;
+    xs_type: any;
+    xs_value: any;
+} | {
+    children: never[];
+    xs_data: {
+        contenteditable: string;
+        allowdrop: string;
+        class: string;
+    };
+    xs_tag: string;
+    xs_type: number;
+};
 export default _default;
