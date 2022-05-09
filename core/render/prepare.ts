99765b7b42878827185a16ece0b1772e8a90d1e5
kabudaaixuexi <kaburda@163.com>
2022-05-09 19:49:45 +0800

    build: build 0.4.7

--- core/render/prepare.ts
+++ core/render/prepare.ts
@@ -1,7 +1,10 @@
 export default (vm: Target) => {
     vm.style = 'display: flex;flex-direction: column;z-index: 1;width: calc(100% - 200px);height: 100%;position: relative;'
-    const devOptions = document.querySelector('.xs-editor-decorate') 
-    const devContent = document.querySelector('.xs-editor-container') 
-    devContent && (devContent.parentNode as Element).removeChild(devContent);
-    devOptions && (devOptions.parentNode as Element).removeChild(devOptions); 
+    vm.innerHTML = ""
+    // const devOptions = document.querySelector('.xs-editor-decorate') 
+    // const devContent = document.querySelector('.xs-editor-container') 
+    // devContent && (devContent.parentNode as Element).removeChild(devContent);
+    // devOptions && (devOptions.parentNode as Element).removeChild(devOptions); 
+    // // 清除水印
+    // devContent && (devContent.parentNode as Element).removeChild(devOptions); 
 }
