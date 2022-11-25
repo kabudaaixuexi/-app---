f4ddfd35e4811d77fd08f38a25f6ce9be2a30de0
kabudaaixuexi <kaburda@163.com>
2022-11-26 00:38:34 +0800

    🚀 deloy: Unspecified iteration

--- lib/utils/index.d.ts
+++ lib/utils/index.d.ts
@@ -1,2 +1,3 @@
 export declare function deepClone(O: Target): Promise<unknown>;
 export declare function debounce(func: any, wait: any, immediate: any): (this: {}) => void;
+export declare const getCookie: (cookie_name: string) => any;
