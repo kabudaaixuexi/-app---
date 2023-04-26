63fee650aa505ff6ff94de6fae9c3601dcc2a39c
ningrui <ningrui@shinemo.com>
2023-04-26 17:30:54 +0800

    🔨 feat: expose

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
+export declare const createDom: (vd: any, vm?: any) => any;
+export declare const createVdom: (vm: any) => any;
 declare const _default: (Element: Element | null, Config?: Configure, Callback?: Function) => Error | undefined;
 export default _default;
