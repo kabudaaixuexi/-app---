a72a9dde2a8fff514c61863a81f52a1f7760beb6
ningrui <ningrui@shinemo.com>
2023-04-26 17:57:10 +0800

    🔨 feat: expose

--- core/index.ts
+++ core/index.ts
@@ -1,9 +1,11 @@
 import renderXsEditor from './render'
 import { achieveD } from './packages/xseditor-dummy/achieveD'
 import { achieveVd } from './packages/xseditor-dummy/achieveVd'
+import request from './packages/xseditor-utils/request'
 
 export const createDom = achieveD
 export const createVdom = achieveVd
+export const uploadFile = request
 export default (
     Element: Element | null,
     Config:Configure = {
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export declare const createDom: (vd: any, vm?: any) => any;
 export declare const createVdom: (vm: any) => any;
+export declare const uploadFile: (props: any) => Promise<unknown>;
 declare const _default: (Element: Element | null, Config?: Configure, Callback?: Function) => Error | undefined;
 export default _default;
